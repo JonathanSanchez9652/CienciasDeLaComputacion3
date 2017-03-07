8c520e8b7e2c126ea868a27bf008941cfacad6fc
JonathanSanchez9652 <jjss9652@hotmail.com>
2017-03-06 20:47:08 -0500

    Creación práctica 4
    Creación práctica 4 variables en expresión

---
+++ Practicas/Practica4 variables/doc.go
@@ -0,0 +1,6 @@
+// Practica4 variables project doc.go
+
+/*
+Practica4 variables document
+*/
+package main
---
+++ Practicas/Practica4 variables/main.go
@@ -0,0 +1,10 @@
+// Practica4 variables project main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
